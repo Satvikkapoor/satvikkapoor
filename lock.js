1727e158ea8e486476a312b9f46eaae3cf1cbbbc
Satvikkapoor <satvikkapoor007@gmail.com>
2024-04-23 23:26:41 -0400

    initial

---
+++ lock.js
@@ -0,0 +1,34 @@
+function magic() {
+    if (localStorage.count){    
+        randomNumber = Math.floor(Math.random() * 4)+1;
+        if (randomNumber == 1) {
+            document.body.style.backgroundColor = "#b66770";   
+        } else if (randomNumber == 2) {
+            document.body.style.backgroundColor = "#6770b6";
+        } else if (randomNumber == 3) {
+            document.body.style.backgroundColor = "#67b6ad";
+        } else{
+            document.body.style.backgroundColor = "#70b667";
+        }
+        localStorage.count = Number(localStorage.count) + 1;
+        document.querySelector("#numbercounter").innerHTML = localStorage.count;
+    } else {
+        localStorage.count = 0;                                           
+        document.querySelector("#numbercounter").innerHTML = localStorage.count;
+    }
+}
+
+function smaller() {
+    document.querySelector("#bro").style.height = "85%";
+    randomNumber2 = Math.floor(Math.random() * 3)+2;
+    document.querySelector("#bro").setAttribute("src", "assets/img/"+String(randomNumber2)+".png");
+}
+
+function larger() {
+    document.querySelector("#bro").style.height = "90%";
+    document.querySelector("#bro").setAttribute("src", "assets/img/1.png");
+}
+if (typeof(Storage) !== "undefined") {
+    document.querySelector("#numbercounter").innerHTML = localStorage.count;
+    console.log(10);
+}
